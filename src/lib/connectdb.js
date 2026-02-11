8fce3a23787c21ac2ae64540965212a1ed1183f0
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-11 16:42:59 +0100

    add the connection to mongodb

---
+++ src/lib/connectdb.js
@@ -0,0 +1,31 @@
+
+
+
+//connectdb.js
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+const connect = async () =>{
+
+    if(isConnected) return;
+
+    try{
+        //
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: process.env.DB_NAME,
+        });
+
+        isConnected = conn.connections[0].readyState === 1;
+        console.log("Mongo connected !");
+
+    } catch(e){
+        console.error("Erreur de connexion MongoDB : ", e.message);
+        throw new Error("Error in connecting");
+    }
+};
+
+
+export default connect;
+
+
