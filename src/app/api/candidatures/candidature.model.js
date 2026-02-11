ba09a2e7a16703217111a6d060d4dcaea207e8f3
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-02-11 19:03:50 +0100

    completion de model de candidature et dans champs du formulaire

--- src/app/api/candidatures/candidature.model.js
+++ src/app/api/candidatures/candidature.model.js
@@ -3,8 +3,35 @@ import mongoose from "mongoose";
 
 const CandidatureSchema = new mongoose.Schema(
     {
-        //
+        entreprise:{
+            type: String,
+            required: true
+        },
+        poste:{
+            type: String,
+            required: true
+        },
+        lien: {
+            type: String
+        },
+        dateEnvoi: {
+            type: Date
+        },
+        //tableau temporaire pour relance envisage de rajoute un model specifique pour
+        relance:{
+            relance1:{type: Date},
+            relance2: {type: Date},
+            relance3: {type: Date}
+        }
+        ,
+        statut: {
+            type: String,
+            enum: ["En attente", "Acceptée", "Refusée"],
+            default: "En attente"
+        }
+    }, {
+        timeStamps: { createdAt: true}
     }
-)
+);
 
 export default mongoose.models.Candidatures || mongoose.model("Candidatures", CandidatureSchema);
---
+++ src/utils/formFields.js
@@ -0,0 +1,54 @@
+
+
+export const inputForm = [
+    {
+    label: 'Entreprise',
+    type: 'text',
+    name: 'entreprise',
+    id: 'entreprise',
+    placeholder: 'le nom de l entreprise',
+    icon: ''
+    },
+    {
+        label: 'Poste',
+        type: 'text',
+        name: 'poste',
+        id: 'poste',
+        plaveholder: "Entrer le nom de l'entreprise",
+        icon: ""
+    },
+    {
+        label: 'Lien',
+        type: 'text',
+        name: 'poste',
+        id: 'poste',
+        plaveholder: "Entrer le lien de la candidature",
+        icon: ""
+    },
+    {
+        label: 'Date d envoi',
+        type: 'date',
+        name: 'dateEnvoi',
+        id: 'dateEnvoi',
+        plaveholder: "Entrer la date d envoi de la candidature",
+        icon: ""
+    },
+    {
+        label: 'Relance',
+        type: 'date',
+        name: 'relance',
+        id: 'relance',
+        plaveholder: "Entrer la date de relance",
+        icon: ""
+    },
+    {
+        label: 'Statut',
+        type: 'select',
+        name: 'statut',
+        id: 'statut',
+        plaveholder: "Entrer le statut",
+        icon: ""
+    }
+];
+
+
